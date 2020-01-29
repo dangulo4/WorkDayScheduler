6f929aa7cdf1702bf820f7f4b34826ba7c592c4f
Daniel Angulo <dangulo4@gmail.com>
2020-01-29 00:47:41 -0800

    inital commit, added html and moment js to js file

---
+++ script.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+
+} );
+
+$('#currentDay').text(moment().format('dddd, MMMM Do'));
+
+
+// show event
+
+    // Clear todoList element and update todoCountSpan
+   
+    // Render a new li for each todo
+    
+ 
+  
+  //initialize events
+  
+    //Get events from localStorage
+    
+    
+    //If events were retreived from localStorage, update the events array to it
+   
+      
+  // When form is submitted...
+  
+   
+  
+  // Return from function early if submitted todoText is blank
+  
+  
+  // Add new events array, clear the input
+  
+  
+  // Store updated todos in localStorage, re-render the list
+ 
