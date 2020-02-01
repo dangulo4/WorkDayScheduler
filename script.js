4db60762f160653adbc7eed32bab0aea1801bdc0
Daniel Angulo <dangulo4@gmail.com>
2020-01-31 22:21:26 -0800

    cleaned up color code logic

--- script.js
+++ script.js
@@ -1,81 +1,55 @@
 $(document).ready(function () {
 
-  var events = [];
-
-  function init() {
-    //Parse the JSON string to an object
-    var storeEvent = JSON.parse(localStorage.getItem('events'));
-    //Update localStorage
-    if (storeEvent !== null) {
-      events = storeEvent;
-    }
-
-  }
-
-  function storeEvent() {
-    localStorage.setItem('events', JSON.stringify(events));
-  }
-
-
-
-  function saveEvent() {
-    $('.saveBtn').on('click', function () {
-      alert
-      storeEvent();
-    });
-  }
-  saveEvent();
-
-  getTime0();
-
-
-  //check for timeblock
-  function addZero(i) {
-    if (i < 10) {
-      i = "0" + i;
-    }
-    return i;
-  }
-
-
-
-
-
-
-  function getTime0() {
-    var d = new Date();
-    var x = document.getElementById('zeroHour');
-    var y = document.getElementById('eventText0');
-    var h = addZero(d.getHours());
-    var m = addZero(d.getMinutes());
-    var s = addZero(d.getSeconds());
- 
-    var checkTimes = document.getElementsByClassName('hour');
-for (var i = 0; i > checkTimes.length ; i++){
+$('.saveBtn').on('click', function (event){
+  alert('Your event has beed saved');
   
-    if (x.innerHTML = h + ":" + m + ":" + s > '09:00' && y.innerText !== null) {
-      x.innerText = '23:00';
-      y.style.addClass('hour');
-    } else if (x.innerHTML = h + ":" + m + ":" + s < '09:00' && y.innerText === null) {
-      y.style.background = 'red';
-      x.innerText = '09:00';
-    } else {
-      x.innerText = ':00';
-      y.style.background.addClass('green');
-    }
-    console.log(checkTimes[i].innerText);
-
+  for (var i = 1; i < 8; i++) {
+    var saveEvents = localStorage.getItem('event' + i)
+    $('#event' + i).val(saveEvents);
+   // localStorage.setItem('events'+i, JSON.stringify(events));
+    //document.getElementById("#event").innerHTML = localStorage.getItem('');
   }
+});
+function getTime() {
+  var hours = 9;
+  var checkTimes = document.querySelectorAll('#eventText');
+  
+  for (var i = 0; i < checkTimes.length; i++){
+    time = parseInt(moment().format('LT'));
+          
+    parseInt(hours);
+    
+    if (moment().format('HHmm' > 1700 )) {
+      
+      
+      $(checkTimes[i]).addClass('past');
+      $(checkTimes[i]).attr("readonly", "readonly");
+      
+     // break;
+
+    } else if (hours > time) {
+      $(checkTimes[i]).addClass('past');
+      $(checkTimes[i]).attr("readonly", "readonly");
+    } 
+    else if (hours = time) {
+      $(checkTimes[i]).addClass('present');
+    } else if (hours < time) {
+      $(checkTimes[i]).addClass('future');
+      
+    }
+    console.log(hours > time);
+    //hr = hr +01;
+    console.log('Momentjs time is: ' + time);
+    console.log('Time is: ' + hours);      
+    console.log(hours > time);
+    hours = hours + 1;
+  }
+  
 }
 
 
-  
+getTime();
 
 
 });
-
-$('#currentDay').text(moment().format('dddd, MMMM Do'));
-
-// $("#click-me").on("click", function() {
-//   alert("I've been clicked!");
-// });
+$('#currentDay').text(moment().format('dddd, MMMM Do')); 
